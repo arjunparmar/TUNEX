0ce6ba3c17c672c047b1c6f8a9c518b796c47f99
Deepanshu Prasad <60698292+dpansu20@users.noreply.github.com>
2021-03-10 20:37:38 +0530

    uploaded static copy

---
+++ Deepanshu/Deploy/frontend/static - Copy/script/tunex.js
@@ -0,0 +1,42 @@
+
+// section 3 static box
+
+
+function showStatic(id) {
+    var box = document.getElementById('staticlinkbox1');
+    var butt = document.getElementById('staticlinkbox2');
+
+    box.style.visibility = 'hidden';
+    butt.style.visibility = 'visible';
+}
+
+function hideStatic(id) {
+    var box = document.getElementById('staticlinkbox1');
+    var butt = document.getElementById('staticlinkbox2');
+
+    butt.style.visibility = 'hidden';
+    box.style.visibility = 'visible';
+}
+
+
+// section 3 live box
+
+
+function showLive(id) {
+    var box = document.getElementById('livelinkbox1');
+    var butt = document.getElementById('livelinkbox2');
+
+    box.style.visibility = 'hidden';
+    butt.style.visibility = 'visible';
+}
+
+function hideLive(id) {
+    var box = document.getElementById('livelinkbox1');
+    var butt = document.getElementById('livelinkbox2');
+
+    butt.style.visibility = 'hidden';
+    box.style.visibility = 'visible';
+}
+
+
+//
